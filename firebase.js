2fc7eb79e2b1961a6c68db9a49a729df7328f41c
Sanket Banerjee <sanketbanerjee.2004@gmail.com>
2022-09-29 20:13:43 +0530

    first commit

---
+++ firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBRhBHS0KjB6IAaHYSJkK64i4pIBnCMiPU",
+    authDomain: "friendslist-8e51e.firebaseapp.com",
+    databaseURL: "https://friendslist-8e51e-default-rtdb.firebaseio.com",
+    projectId: "friendslist-8e51e",
+    storageBucket: "friendslist-8e51e.appspot.com",
+    messagingSenderId: "746552689385",
+    appId: "1:746552689385:web:55290b87262520f37ba678"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
